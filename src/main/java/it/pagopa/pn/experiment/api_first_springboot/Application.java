d318d4e2c8f5e886be022ae1ccd95225dce2c8a4
marco-vit-pagopa <marco.vit@pagopa.it>
2022-02-01 13:13:38 +0100

    Personalizzazione path

--- src/main/java/it/pagopa/pn/experiment/api_first_springboot/Application.java
+++ src/main/java/it/pagopa/pn/experiment/api_first_springboot/Application.java
@@ -1,9 +1,11 @@
 package it.pagopa.pn.experiment.api_first_springboot;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,15 +21,20 @@ public class Application {
     }
 
     @RestController
-    @RequestMapping("/example/")
+    @RequestMapping("/{path}/")
     public static class RootController {
 
+        @Value("${required.root.path}")
+        private String configuredRootPath;
+
         @GetMapping("info")
-        public Map<String, ?> info() {
+        public Map<String, ?> info(@PathVariable("path") String path) {
             Map<String, Object> info = new HashMap<>();
             info.put( "env", System.getenv() );
             info.put( "now", Instant.now().toString() );
-            info.put( "name", "app2");
+            info.put( "configured-root-path", this.configuredRootPath);
+            info.put( "request-path", path);
+            info.put( "name", "app_2022_02_01_12_05");
             return info;
         }
     }
