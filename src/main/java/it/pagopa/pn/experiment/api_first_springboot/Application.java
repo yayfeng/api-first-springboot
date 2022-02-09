861a715da0ad0fb0b805c971b7ae9ed182d66992
marco-vit-pagopa <marco.vit@pagopa.it>
2022-02-09 02:54:46 +0100

    Add custom DNS

--- src/main/java/it/pagopa/pn/experiment/api_first_springboot/Application.java
+++ src/main/java/it/pagopa/pn/experiment/api_first_springboot/Application.java
@@ -4,10 +4,7 @@ package it.pagopa.pn.experiment.api_first_springboot;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.Instant;
 import java.util.HashMap;
@@ -28,9 +25,13 @@ public class Application {
         private String configuredRootPath;
 
         @GetMapping("info")
-        public Map<String, ?> info(@PathVariable("path") String path) {
+        public Map<String, ?> info(
+                @PathVariable("path") String path,
+                @RequestHeader Map<String, String> headers
+        ) {
             Map<String, Object> info = new HashMap<>();
             info.put( "env", System.getenv() );
+            info.put( "headers", headers );
             info.put( "now", Instant.now().toString() );
             info.put( "configured-root-path", this.configuredRootPath);
             info.put( "request-path", path);
